72859781d2cbca543e63e45c277e5628d1da96df
akihiro-kato-rera <tainakaritu19910821@yahoo.co.jp>
2025-07-27 11:14:11 +0900

    レッスン３４チャプター７の課題

--- src/main/java/com/techacademy/controller/ReportsController.java
+++ src/main/java/com/techacademy/controller/ReportsController.java
@@ -3,13 +3,17 @@ package com.techacademy.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.techacademy.entity.Reports;
 import com.techacademy.service.ReportsService;
+import com.techacademy.service.UserDetail;
 
 @Controller
 @RequestMapping("reports")
@@ -33,6 +37,21 @@ public class ReportsController {
         return "reports/list";
     }
 
+    // 日報詳細画面
+    @GetMapping(value = "/{id}/")
+    public String detail(@PathVariable("id") Integer id, Model model) {
 
+        model.addAttribute("reports", reportsService.findById(id));
+        return "reports/detail";
+    }
 
+    // 日報新規登録画面
+    @GetMapping(value = "/add")
+    public String create(@ModelAttribute Reports reports,Model model,@AuthenticationPrincipal UserDetail userDetail) {
+
+        String loginUserName = userDetail.getEmployee().getName();
+        model.addAttribute("loginUserName",loginUserName);
+
+        return "reports/new";
+    }
  }
--- src/main/java/com/techacademy/entity/Reports.java
+++ src/main/java/com/techacademy/entity/Reports.java
@@ -5,14 +5,18 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.SQLRestriction;
 import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -37,10 +41,13 @@ public class Reports {
 
     // ID
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     // 日付
+    @NotNull
     @Column(nullable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate reportDate;
 
     // タイトル
